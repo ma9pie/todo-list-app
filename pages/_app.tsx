5dec721df94d1e73b61b71dd5a9e62e17bc77b0f
ma9pie <ma9pie.dev@gmail.com>
2023-02-26 19:37:51 +0900

    icon 및 css 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/app.scss";
+
+import type { AppProps } from "next/app";
+import { Fragment } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,115 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        <meta name="description" content="Todo List App" />
+
+        {/* 아이콘 생성 사이트 - https://favicomatic.com */}
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Todo List App" />
+        <meta name="application-name" content="Todo List App" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta
+          name="msapplication-square70x70logo"
+          content="icons/mstile-70x70.png"
+        />
+        <meta
+          name="msapplication-TileImage"
+          content="icons/mstile-144x144.png"
+        />
+        <meta
+          name="msapplication-square150x150logo"
+          content="icons/mstile-150x150.png"
+        />
+        <meta
+          name="msapplication-wide310x150logo"
+          content="icons/mstile-310x150.png"
+        />
+        <meta
+          name="msapplication-square310x310logo"
+          content="icons/mstile-310x310.png"
+        />
+        <link rel="manifest" href="manifest.json" />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="icons/apple-touch-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="icons/apple-touch-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="icons/apple-touch-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="icons/apple-touch-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="icons/apple-touch-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="icons/apple-touch-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="icons/apple-touch-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="icons/apple-touch-icon-152x152.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="icons/favicon-16x16.png"
+          sizes="16x16"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="icons/favicon-32x32.png"
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="icons/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="icons/favicon-128.png"
+          sizes="128x128"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="icons/favicon-196x196.png"
+          sizes="196x196"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
