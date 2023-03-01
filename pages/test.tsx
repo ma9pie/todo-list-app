54ed55e4f45874206476e914e6a8699ee9e82003
ma9pie <ma9pie.dev@gmail.com>
2023-03-01 14:58:00 +0900

    컴포넌트 위치 변경

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,8 +1,8 @@
 import { css } from "@emotion/css";
 import styled from "@emotion/styled";
 
+import Theme from "@/components/Theme";
 import Check from "@/images/check.svg";
-import Theme from "@/shared/Theme";
 
 export default function Test() {
   return (
