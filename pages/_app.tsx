1ee942d4960b39e25b62a6ecd685f5986ba25aff
ma9pie <ma9pie.dev@gmail.com>
2023-03-03 19:11:45 +0900

    구글 애널리틱스 연동

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,9 @@ import "@/styles/app.scss";
 
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import type { ReactElement, ReactNode } from "react";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -14,7 +15,22 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const router = useRouter();
   const getLayout = Component.getLayout ?? ((page) => page);
+
+  // 구글 애널리틱스
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      // @ts-ignore
+      window.gtag("config", process.env.NEXT_PUBLIC_GA_TRACKING_ID, {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return <Fragment>{getLayout(<Component {...pageProps} />)}</Fragment>;
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -148,6 +148,26 @@ export default function Document() {
             `,
           }}
         ></script>
+
+        {/* 구글 애널리틱스 */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+        />
+        <script
+          id="google-analytics"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
       <body>
         <div id="modal"></div>
