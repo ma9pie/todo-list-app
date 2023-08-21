8221fdc0ee9d6349e8da9c3b47e49903e00157ba
ma9pie <ma9pie.dev@gmail.com>
2023-08-21 11:17:23 +0900

    build : 환경변수 수정

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,11 +2,10 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
-  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID!,
-      clientSecret: process.env.GOOGLE_SECRET!,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_ID!,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET!,
     }),
   ],
 });
