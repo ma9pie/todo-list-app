6601d30c08ae9ea794d17eda008940e50d7bb5f5
ma9pie <ma9pie.dev@gmail.com>
2023-08-21 10:32:06 +0900

    text : next-auth production 테스트

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
+  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_ID!,
