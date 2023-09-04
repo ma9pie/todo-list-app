04bf0308d044d65e505dee9af3f613966ddfbe09
ma9pie <ma9pie.dev@gmail.com>
2023-09-05 03:43:46 +0900

    refactor : 상수화

--- constants/index.ts
+++ constants/index.ts
@@ -9,7 +9,11 @@ export const COLOR_LIST = [
   "#d1d6db",
   "#4d4d59",
 ];
-
 export const INQUIRY_URL =
   "https://docs.google.com/forms/d/1eE3KBOtAmtNh5cLHlGyrZC7q5I_rvG0TxwaJ16UiuvI";
 export const GITHUB_URL = "https://github.com/ma9pie/todo-list-app";
+export const FIREBASE_POLICY_URL =
+  "https://firebase.google.com/policies/analytics/";
+export const SENTRY_PRIVACY_URL = "https://sentry.io/privacy/";
+export const APP_PRIVACY_POLICY_GENERATOR_URL =
+  "https://app-privacy-policy-generator.nisrulz.com/";
