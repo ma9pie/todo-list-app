49018ae915d53dd6aa040e2271ba5c0d85d82f3e
ma9pie <ma9pie.dev@gmail.com>
2023-09-10 15:18:54 +0900

    fix : CheckBox 컴포넌트 수정

--- components/shared/CheckBox.tsx
+++ components/shared/CheckBox.tsx
@@ -5,25 +5,21 @@ import CheckedSvg from "@/images/checked.svg";
 import UncheckedSvg from "@/images/unchecked.svg";
 
 interface Props {
-  className?: string;
   checked?: boolean;
+  onClick?: () => void;
 }
 
-const CheckBox = ({ className, checked }: Props) => {
-  if (checked)
-    return (
-      <Span>
-        <CheckedSvg className={className}></CheckedSvg>
-      </Span>
-    );
-  else
-    return (
-      <Span>
-        <UncheckedSvg className={className}></UncheckedSvg>
-      </Span>
-    );
+const CheckBox = ({ checked, onClick = () => {} }: Props) => {
+  return (
+    <Span onClick={onClick}>
+      {checked ? <CheckedSvg></CheckedSvg> : <UncheckedSvg></UncheckedSvg>}
+    </Span>
+  );
 };
 
 export default CheckBox;
 
-const Span = styled.span``;
+const Span = styled.span`
+  display: flex;
+  align-items: center;
+`;
