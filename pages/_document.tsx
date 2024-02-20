090df67f8746a12a54a9c95f5ae98ea6644f1c41
ma9pie <ma9pie.dev@gmail.com>
2024-02-21 03:07:16 +0900

    feat : vercel analytics 적용

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -168,6 +169,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Analytics></Analytics>
       </body>
     </Html>
   );
